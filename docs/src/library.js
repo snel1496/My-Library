f013f47f6adbd88e5a7ff1b872ef7760b1fadd3d
snel <stsam63@gmail.com>
2025-10-15 18:03:25 -0500

    use fetch api

--- docs/src/library.js
+++ docs/src/library.js
@@ -1,20 +1,19 @@
-
-var reader = new FileReader();
 var table = document.getElementById('library');
 
-reader.readAsText("../data/library.csv");
-reader.onloadend = () => {
-    table.innerHTML = "";
-    for (let row of CSV.parse(reader.result)) {
+fetch("../data/library.csv")
+    .then((res) => res.text())
+    .then((csvText) => {
+        table.innerHTML = "";
+        for (let row of CSV.parse(csvText)) {
 
-        // let tr = table.insertRow();
-        // var isbn = 9780804172066;
-        // tr.insertCell().innerHtml = `<img src="https://covers.openlibrary.org/b/isbn/${isbn}-S.jpg" />`;
-        // tr.insertCell
+            // let tr = table.insertRow();
+            // var isbn = 9780804172066;
+            // tr.insertCell().innerHtml = `<img src="https://covers.openlibrary.org/b/isbn/${isbn}-S.jpg" />`;
+            // tr.insertCell
 
-        for (let col of row) {
-            let td = tr.insertCell();
-            td.innerHTML = col;
+            for (let col of row) {
+                let td = tr.insertCell();
+                td.innerHTML = col;
+            }
         }
-    }
-}
+    })
