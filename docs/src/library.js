f608989d57c5c367a20cfcdac005db5e4acf8904
snel <stsam63@gmail.com>
2025-10-15 20:56:34 -0500

    only pull certain columns

--- docs/src/library.js
+++ docs/src/library.js
@@ -1,13 +1,26 @@
-var table = document.getElementById("library");
+var table = document.getElementById(library);
+var keepCols = { Title: -1, Subtitle: -1, Series: -1, Volume: -1, Author: -1, 'Date Published': -1, 'Original Date Published': -1, Edition: -1, Genre: -1, Summary: -1, 'Word Count': -1, 'Number of Pages': -1, ISBN: -1, 'Date Added': -1 }
+
 fetch("https://raw.githubusercontent.com/snel1496/My-Library/refs/heads/main/docs/data/library.csv")
     .then(res => res.text())
-    .then(csv => {
+    .then(csvRaw => {
         table.innerHTML = "";
-        for (let row of CSV.parse(csv)) {
+        let csvParsed = CSV.parse(csvRaw);
+        for (let i = 0; i < csvParsed.length; i++) {
             let tr = table.insertRow();
-            for (let col of row) {
-                let td = tr.insertCell();
-                td.innerHTML = col;
+            if (i == 0) {
+                for (let j = 0; j < row.length; j++) {
+                    if (keepCols[row[j]]) {
+                        keepCols[row[j]] = j;
+                        let td = tr.insertCell();
+                        td.innerHTML = csvParsed[i][j];
+                    }
+                }
+            } else {
+                for (const [_, value] of Object.entries(keepCols)) {
+                    let td = tr.insertCell();
+                    td.innerHTML = csvParsed[i][value];
+                }
             }
         }
     });
