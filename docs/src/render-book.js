311f706a96d26d313247944dd82c61aced33a200
snel1496 <stsam63@gmail.com>
2025-10-29 16:51:42 -0500

    start of book page

---
+++ docs/src/render-book.js
@@ -0,0 +1,9 @@
+
+
+/**
+ * This will create an html rendered version of the book data 
+ * @param {string[][]} bookData - 2d array where the first array is length 2, and contains column names and value names ex: [["Title", "Author"], ["Mistborn", "Brandon Sanderson"]]
+ */
+function render(bookData) {
+
+}
