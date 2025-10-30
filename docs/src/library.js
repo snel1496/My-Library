0eebd4406bcda61fd01c72ae3bd619809cda21c6
snel <stsam63@gmail.com>
2025-10-29 20:59:00 -0500

    added individual book view

--- docs/src/library.js
+++ docs/src/library.js
@@ -5,7 +5,8 @@ var seriesSet = new Set();
 var authorDropdown = document.getElementById("author-dropdown");
 var authorSet = new Set();
 
-var keepCols = { Title: -1, Subtitle: -1, Series: -1, Volume: -1, Author: -1, 'Date Published': -1, 'Word Count': -1, Genre: -1, 'Number of Pages': -1, ISBN: -1, 'Date Added': -1 }
+var tableCols = { Title: -1, Subtitle: -1, Series: -1, Volume: -1, Author: -1, 'Date Published': -1, Genre: -1, 'Number of Pages': -1, ISBN: -1, 'Date Added': -1 }
+var additionalCols = { Summary: -1 }
 
 fetch("https://raw.githubusercontent.com/snel1496/My-Library/refs/heads/main/docs/data/library.csv") // TODO this function should probably be split up
     .then(res => res.text())
@@ -19,9 +20,9 @@ fetch("https://raw.githubusercontent.com/snel1496/My-Library/refs/heads/main/doc
 // configure MiniSearch https://github.com/lucaong/minisearch
 //Stuff to do after the data has been parsed and processed
 function onReady() {
-    configDropDown(seriesDropdown, seriesSet, keepCols.Series);
-    configDropDown(authorDropdown, authorSet, keepCols.Author);
-    searchBox.addEventListener('input', () => filterColumnByString(searchBox.value, keepCols.Title));
+    configDropDown(seriesDropdown, seriesSet, tableCols.Series);
+    configDropDown(authorDropdown, authorSet, tableCols.Author);
+    searchBox.addEventListener('input', () => filterColumnByString(searchBox.value, tableCols.Title));
 }
 
 function configDropDown(dropdownElement, dropdownSet, filterColumnIdx) {
@@ -58,24 +59,27 @@ function configureTable(libraryData) {
             let tr = thead.insertRow();
             tr.insertCell().innerHTML = "Image"; // add space for image
             for (let j = 0; j < libraryData[i].length; j++) {
-                if (keepCols[libraryData[i][j]]) {
-                    keepCols[libraryData[i][j]] = j;
+                if (tableCols[libraryData[i][j]]) {
+                    tableCols[libraryData[i][j]] = j;
                     let td = tr.insertCell();
                     td.innerHTML = `${libraryData[i][j]}<button class="btn" onclick="sortTableByColumn(${j})"><i class="fa fa-sort"></i></button>`; // TODO do something to change the icon to fa-sort-down or fa-sort-up when appropriate, it also looks weird on the site
+                } else if (additionalCols[libraryData[i][j]]) {
+                    additionalCols[libraryData[i][j]] = j;
                 }
             }
         } else {
             let tr = tbody.insertRow();
+            tr.onclick = () => { renderBookPage(libraryData[i]) }
             let td = tr.insertCell();
-            td.innerHTML = `<img src="https://covers.openlibrary.org/b/isbn/${libraryData[i][keepCols.ISBN]}-S.jpg" />`
-            for (const [_, idx] of Object.entries(keepCols)) {
+            td.innerHTML = `<img src=${getImageUrl(libraryData[i][tableCols.ISBN], 'S')} alt="BookImage if available">`;
+            for (const [_, idx] of Object.entries(tableCols)) {
                 let td = tr.insertCell();
                 td.innerHTML = libraryData[i][idx];
                 switch (idx) {
-                    case keepCols.Author:
+                    case tableCols.Author:
                         authorSet.add(libraryData[i][idx]);
                         break;
-                    case keepCols.Series:
+                    case tableCols.Series:
                         seriesSet.add(libraryData[i][idx]);
                         break;
                     default:
@@ -84,6 +88,26 @@ function configureTable(libraryData) {
             }
         }
     }
+}
+
+function renderBookPage(dataRow) {
+    document.getElementById("book-view").style = "display: block";
+    document.getElementById("bImage").src = getImageUrl(dataRow[tableCols.ISBN], 'M'); // TODO change url if screen gets too small
+    document.getElementById("bLink").href = `https://openlibrary.org/isbn/${dataRow[tableCols.ISBN]}`;
+    document.getElementById("bTitle").innerHTML = dataRow[tableCols.Title];
+    document.getElementById("bAuthor").innerHTML = `Author: ${dataRow[tableCols.Author]}`;
+    document.getElementById("bSubtitle").innerHTML = dataRow[tableCols.Subtitle];
+    document.getElementById("bSeries").innerHTML = `Series ${dataRow[tableCols.Series]}`;
+    document.getElementById("bVolume").innerHTML = `Volumn: ${dataRow[tableCols.Volume]}`;
+    document.getElementById("bGenre").innerHTML = `Genre: ${dataRow[tableCols.Genre]}`;
+    document.getElementById("bSummary").innerHTML = dataRow[additionalCols.Summary];
+    document.getElementById("bPublishedDate").innerHTML = `Published: ${dataRow[tableCols["Date Published"]]}`;
+    document.getElementById("bDateAdded").innerHTML = `Date Added To Collection: ${dataRow[tableCols["Date Added"]]}`;
+    document.getElementById("bIsbn").innerHTML = `ISBN: ${dataRow[tableCols.ISBN]}`;
+}
+
+function getImageUrl(isbn, size) {
+    return `https://covers.openlibrary.org/b/isbn/${isbn}-${size}.jpg`;
 }
 function sortTableByColumn(columnIdx) {
     var rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
